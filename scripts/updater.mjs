dc8831aad29598deebc00f96f285c0f4b12a11f4
ezequielnick <107352853+ezequielnick@users.noreply.github.com>
2025-08-30 22:59:50 +0800

    chore: udpate download link

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -1,28 +1,47 @@
 import yaml from 'yaml'
 import { readFileSync, writeFileSync } from 'fs'
+import { execSync } from 'child_process'
 
 const pkg = readFileSync('package.json', 'utf-8')
 let changelog = readFileSync('changelog.md', 'utf-8')
-const { version } = JSON.parse(pkg)
-const downloadUrl = `https://github.com/mihomo-party-org/mihomo-party/releases/download/v${version}`
+let { version } = JSON.parse(pkg)
+
+// 获取 commit hash
+function getGitCommitHash() {
+  try {
+    return execSync('git rev-parse --short=7 HEAD').toString().trim()
+  } catch (error) {
+    console.warn('无法获取 Git commit hash，使用默认值')
+    return 'unknown'
+  }
+}
+
+if (version.endsWith('-dev')) {
+  const commitHash = getGitCommitHash()
+  version = version.replace('-dev', `-${commitHash}-dev`)
+}
+const isDevVersion = version.includes('-dev')
+const downloadUrl = isDevVersion 
+  ? `https://github.com/mihomo-party-org/clash-party/releases/download/dev`
+  : `https://github.com/mihomo-party-org/clash-party/releases/download/v${version}`
 const latest = {
   version,
   changelog
 }
 
 changelog += '\n### 下载地址：\n\n#### Windows10/11：\n\n'
-changelog += `- 安装版：[64位](${downloadUrl}/mihomo-party-windows-${version}-x64-setup.exe) | [32位](${downloadUrl}/mihomo-party-windows-${version}-ia32-setup.exe) | [ARM64](${downloadUrl}/mihomo-party-windows-${version}-arm64-setup.exe)\n\n`
-changelog += `- 便携版：[64位](${downloadUrl}/mihomo-party-windows-${version}-x64-portable.7z) | [32位](${downloadUrl}/mihomo-party-windows-${version}-ia32-portable.7z) | [ARM64](${downloadUrl}/mihomo-party-windows-${version}-arm64-portable.7z)\n\n`
+changelog += `- 安装版：[64位](${downloadUrl}/clash-party-windows-${version}-x64-setup.exe) | [32位](${downloadUrl}/clash-party-windows-${version}-ia32-setup.exe) | [ARM64](${downloadUrl}/clash-party-windows-${version}-arm64-setup.exe)\n\n`
+changelog += `- 便携版：[64位](${downloadUrl}/clash-party-windows-${version}-x64-portable.7z) | [32位](${downloadUrl}/clash-party-windows-${version}-ia32-portable.7z) | [ARM64](${downloadUrl}/clash-party-windows-${version}-arm64-portable.7z)\n\n`
 changelog += '\n#### Windows7/8：\n\n'
-changelog += `- 安装版：[64位](${downloadUrl}/mihomo-party-win7-${version}-x64-setup.exe) | [32位](${downloadUrl}/mihomo-party-win7-${version}-ia32-setup.exe)\n\n`
-changelog += `- 便携版：[64位](${downloadUrl}/mihomo-party-win7-${version}-x64-portable.7z) | [32位](${downloadUrl}/mihomo-party-win7-${version}-ia32-portable.7z)\n\n`
+changelog += `- 安装版：[64位](${downloadUrl}/clash-party-win7-${version}-x64-setup.exe) | [32位](${downloadUrl}/clash-party-win7-${version}-ia32-setup.exe)\n\n`
+changelog += `- 便携版：[64位](${downloadUrl}/clash-party-win7-${version}-x64-portable.7z) | [32位](${downloadUrl}/clash-party-win7-${version}-ia32-portable.7z)\n\n`
 changelog += '\n#### macOS 11+：\n\n'
-changelog += `- PKG：[Intel](${downloadUrl}/mihomo-party-macos-${version}-x64.pkg) | [Apple Silicon](${downloadUrl}/mihomo-party-macos-${version}-arm64.pkg)\n\n`
+changelog += `- PKG：[Intel](${downloadUrl}/clash-party-macos-${version}-x64.pkg) | [Apple Silicon](${downloadUrl}/clash-party-macos-${version}-arm64.pkg)\n\n`
 changelog += '\n#### macOS 10.15+：\n\n'
-changelog += `- PKG：[Intel](${downloadUrl}/mihomo-party-catalina-${version}-x64.pkg) | [Apple Silicon](${downloadUrl}/mihomo-party-catalina-${version}-arm64.pkg)\n\n`
+changelog += `- PKG：[Intel](${downloadUrl}/clash-party-catalina-${version}-x64.pkg) | [Apple Silicon](${downloadUrl}/clash-party-catalina-${version}-arm64.pkg)\n\n`
 changelog += '\n#### Linux：\n\n'
-changelog += `- DEB：[64位](${downloadUrl}/mihomo-party-linux-${version}-amd64.deb) | [ARM64](${downloadUrl}/mihomo-party-linux-${version}-arm64.deb)\n\n`
-changelog += `- RPM：[64位](${downloadUrl}/mihomo-party-linux-${version}-x86_64.rpm) | [ARM64](${downloadUrl}/mihomo-party-linux-${version}-aarch64.rpm)`
+changelog += `- DEB：[64位](${downloadUrl}/clash-party-linux-${version}-amd64.deb) | [ARM64](${downloadUrl}/clash-party-linux-${version}-arm64.deb)\n\n`
+changelog += `- RPM：[64位](${downloadUrl}/clash-party-linux-${version}-x86_64.rpm) | [ARM64](${downloadUrl}/clash-party-linux-${version}-aarch64.rpm)`
 
 changelog += '\n\n### 机场推荐：\n- 高性能海外机场，稳定首选：[https://狗狗加速.com](https://party.dginv.click/#/register?code=ARdo0mXx)'
 
